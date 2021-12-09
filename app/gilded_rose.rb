7c76b93703e98bc230c182e69303cac46010c6d2
armanirmr <armanirmr@gmail.com>
2021-12-09 17:13:58 -0600

    Methods' definition
    We defined all of our functions in order to improve our code's performance
    
    Co-authored-by: Villaedi <ediberto.sanchez.villalobos@gmail.com>
    Co-authored-by: rene3255 <ranchoeltecal@gmail.com>

--- app/gilded_rose.rb
+++ app/gilded_rose.rb
@@ -1,10 +1,13 @@
+require_relative 'items'
+require_relative 'methods'
+
 # frozen_string_literal: true
 
 class GildedRose
     def initialize(items)
       @items = items
     end
-  
+
     def update_quality()
       @items.each do |item|
         if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
@@ -51,19 +54,5 @@ class GildedRose
           end
         end
       end
-    end
-  end
-  
-  class Item
-    attr_accessor :name, :sell_in, :quality
-  
-    def initialize(name, sell_in, quality)
-      @name = name
-      @sell_in = sell_in
-      @quality = quality
-    end
-  
-    def to_s()
-      "#{@name}, #{@sell_in}, #{@quality}"
     end
   end
---
+++ app/methods.rb
@@ -0,0 +1,51 @@
+require_relative 'items'
+
+# frozen_string_literal: true
+
+class Methods
+    attr_accessor :item
+
+    def initialize(item)
+        @item = item
+    end
+    
+    def AgedBrie
+        item.quality += 1
+    end
+
+    def tenOrLess
+        if item.quality < 50
+            item.quality +=2
+        end
+    end
+    
+    def fiveOrLess
+        if item.sell_in <= 5
+            item.quality +=3
+        end
+    end
+
+    def incrementQ
+        item.quality += 1
+    end
+    
+    def decrementQ
+        item.quality -= 1
+    end
+
+    def limit
+        if item.quality <= 50
+            return true
+        else
+            false
+        end
+    end
+
+    def expiration
+        item.quality += 2
+    end
+
+    def conjured
+        item.quality -= 2
+    end
+end
